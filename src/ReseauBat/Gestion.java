43bbf19c6b107b659a2e96fc9fe3632796b92944
clementche <clement.cheron@wanadoo.fr>
2016-03-14 14:56:57 +0100

    commit2

--- src/ReseauBat/Gestion.java
+++ src/ReseauBat/Gestion.java
@@ -29,7 +29,8 @@ public class Gestion {
 	
 	public Boolean getLum1(){
 		try {
-			return pc.readBool(new GroupAddress("0/1/1"));
+			Thread.sleep(150);
+			return pc.readBool(new GroupAddress("0/2/1"));
 		} catch (KNXFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -46,10 +47,142 @@ public class Gestion {
 
 	}
 	
+	public Boolean getLum2(){
+		try {
+			Thread.sleep(150);
+			return pc.readBool(new GroupAddress("0/2/2"));
+		} catch (KNXFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (KNXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+	public Boolean getLum3(){
+		try {
+			Thread.sleep(150);
+			return pc.readBool(new GroupAddress("0/2/3"));
+		} catch (KNXFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (KNXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+	public Boolean getLum4(){
+		try {
+			Thread.sleep(150);
+			return pc.readBool(new GroupAddress("0/2/4"));
+		} catch (KNXFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (KNXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+	
+	
 	public void switchStateLum1(boolean etat){
 		try {
 			
-			pc.write(new GroupAddress("0/1/1"), etat);
+			try {
+				pc.write(new GroupAddress("0/1/1"), etat);
+				Thread.sleep(150);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (KNXTimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KNXLinkClosedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KNXFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void switchStateLum2(boolean etat){
+		try {
+			
+			try {
+				pc.write(new GroupAddress("0/1/2"), etat);
+				Thread.sleep(150);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (KNXTimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KNXLinkClosedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KNXFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void switchStateLum3(boolean etat){
+		try {
+			
+			try {
+				pc.write(new GroupAddress("0/1/3"), etat);
+				Thread.sleep(150);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (KNXTimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KNXLinkClosedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KNXFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void switchStateLum4(boolean etat){
+		try {
+			
+			try {
+				pc.write(new GroupAddress("0/1/4"), etat);
+				Thread.sleep(150);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		} catch (KNXTimeoutException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -119,6 +252,17 @@ public class Gestion {
 	
 	public void chenillar(int speed, boolean start){
 		// ecrire la fonction chenillar 
+		while(start){
+			this.switchStateLum1(true);
+			this.switchStateLum2(true);
+			this.switchStateLum1(false);
+			this.switchStateLum3(true);
+			this.switchStateLum2(false);
+			this.switchStateLum4(true);
+			this.switchStateLum3(false);
+			this.switchStateLum1(true);
+			this.switchStateLum4(false);
+		}
 	}
 	
 }
